b23a9b2eea9f2154e80bb054619f24b88c73e075
Andrew <andrewrob117@gmail.com>
2026-03-19 22:48:34 -0400

    Enhance settings API response to include photos and party members data, improving the overall functionality of the RSVP endpoint.

--- server.js
+++ server.js
@@ -76,6 +76,8 @@ app.get('/api/settings/rsvp', async (_req, res) => {
   res.json({
     rsvpOpenGlobal: settings.rsvpOpenGlobal !== false,
     events: events.map((e) => ({ id: e.id, name: e.name, rsvpOpen: e.rsvpOpen !== false })),
+    photos: settings.photos || {},
+    partyMembers: settings.partyMembers || {},
   });
 });
 
